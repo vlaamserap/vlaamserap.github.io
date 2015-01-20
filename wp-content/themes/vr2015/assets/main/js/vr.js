cbb6456d71c52158b4a213d82839b8f6e75c5d3f
vlaamserap <vlaamserap@gmail.com>
2015-01-20 16:16:58 +0100

    js

--- wp-content/themes/vr2015/assets/main/js/vr.js
+++ wp-content/themes/vr2015/assets/main/js/vr.js
@@ -228,7 +228,7 @@ jQuery(document).ready(function($) {
 
 	var wdgt = '<div class="list-group-item"><p><a class="btn btn-default btn-xs btn-block" href="https://github.com/vlaamserap/vlaamserap.github.io/issues" role="button">Meld een probleem</a></p></div>';
 
-	$( '.widget-vr .panel-body' ).append( wdgt );
+	$( '#secondary .widget-vr .panel-body' ).append( wdgt );
 
 });
 
